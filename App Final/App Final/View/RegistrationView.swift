168f324f13537223284c6666f4471432f17df3a4
Oalvarado031 <165835512+Oalvarado031@users.noreply.github.com>
2025-03-07 23:08:38 -0600

    1 update

---
+++ App Final/App Final/View/RegistrationView.swift
@@ -0,0 +1,18 @@
+//
+//  RegistrationView.swift
+//  App Final
+//
+//  Created by Oscar Alvarado on 7/3/25.
+//
+
+import SwiftUI
+
+struct RegistrationView: View {
+    var body: some View {
+        Text("Registrarse")
+    }
+}
+
+#Preview {
+    RegistrationView()
+}
